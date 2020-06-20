1a824e0ccdf6b6cd679b84af0551f4f9732dbe59
Kendrick Salting <37995498+codebyksalting@users.noreply.github.com>
2020-06-20 22:39:56 +0800

    Complete the working draft of the script

--- js/accrdion.js
+++ js/accrdion.js
@@ -5,80 +5,65 @@
         var configuration = $.extend({
             openFirstByDefault: true, // true, false
             displayStyle: 'single', // single, any
-            toggleSpeed: 'fast' // instant, slow, medium, fast
+            toggleSpeed: 400 // milliseconds, slow, fast
         }, options );
 
-        // Hide all except the first item when openByDefault is set to 'first
-        function closeFunc( $_obj, $_index = -1 ) {
-            $_needle = $_obj.find( '.accrdion-entry' );
-
-            $_needle.each(function( index ) {
-                $_obj_entry = $(this);
-                $_flag = true;
-                
-                if ( configuration.openByDefault && $_index == -1 ) {
-                    $_flag = false;
-
-                    if ( index == 0 ) {
-                        $_needle.addClass('accrdion-active');
-                    }
-
-                    if ( configuration.displayStyle === 'single' && index > 0 ) {
-                        $_flag = true;
-                    }
-                }
-
-                //console.log( $_index, index ); 
-
-                if ( $_flag ) {
-                    if ( $_obj_entry.hasClass('accrdion-active') ) {
-                        $_obj_entry.removeClass('accrdion-active');
-                    }
-
-                    // Use hide() for instant toggle
-                    if ( configuration.toggleSpeed === 'instant' ) {
-                        $_obj_entry.find('.accrdion-content').hide();
-                    } else {
-                        $_obj_entry.find('.accrdion-content').slideUp( configuration.toggleSpeed );
-                    }
-                }
-            });
-
-            // if ( $_index < 0 ) {
-            //     $_index++;
-            // }
-
-            // $_needle.eq( $_index ).addClass('accrdion-active');
-            console.log(  $_obj.find( '.accrdion-entry' ), $_index );
-            if ( !$_needle.eq( $_index ).hasClass('accrdion-active') ) {
-                $_needle.addClass('accrdion-active');
-
-                // Use show() for instant toggle
-                if ( configuration.toggleSpeed === 'instant' ) {
-                    $_needle.eq( $_index ).find('.accrdion-content').show();
-                } else {
-                    $_needle.eq( $_index ).find('.accrdion-content').slideDown( configuration.toggleSpeed );
-                }
-            }
-        }
-
-        return this.each(function() {
-            const $_accr_wrapper = $(this);
+        return this.each( function() {
+            const $_accrdion_wrapper = $(this);
+            const $_accrdion_item = $_accrdion_wrapper.find( '.accrdion-entry' );
 
             // Add class for styling purposes
-            $_accr_wrapper.addClass( `accrdion-wrapper ${configuration.displayStyle}` );          
+            $_accrdion_wrapper.addClass( `accrdion-wrapper ${configuration.displayStyle}` );          
 
             // Set initial behavior 
-            closeFunc( $_accr_wrapper );
+            $_accrdion_item.each( function( index ) {
+                $_item = $(this);
+              
+                if ( configuration.openFirstByDefault && index == 0 ) {
+                    $_item.addClass( 'accrdion-active' );
+                } else {
+                    $_item.find( '.accrdion-content' ).slideUp( configuration.toggleSpeed );
+                }
+            });
  
-            // Add the event handler and pass the current item index
-            $_accr_wrapper.on('click', '.accrdion-header', function(event) {
-                const $_accr_clicked = $(this);
-                const $_accr_index = $_accr_clicked.parent('.accrdion-entry').index();
+            // Main event handler
+            $_accrdion_wrapper.on( 'click', '.accrdion-header', function( event ) {
+                const $_clicked = $(this);
+                const $_item = $_clicked.parent();
+                const $_wrapper = $_item.parent();
+                const $_index = $_item.index();
+                
+                let $_flag = false;              
 
-                closeFunc( $_accr_wrapper, $_accr_index );
+                if ( configuration.displayStyle === 'single' ) {
+                    // Cycle through all of the accordion items
+                    $_wrapper.find( '.accrdion-entry' ).each( function( index ) {
+                        $_current_item = $(this);
+
+                        // Enable toggle if openFirstByDefault: true and displayStyle: single
+                        if ( $_index == index && !$_current_item.hasClass( 'accrdion-active' ) ) {
+                            $_flag = true;
+                        } else {
+                            // Nothing should happen if the current active accordion item's header is clicked
+                            if ( $_index != index ) {
+                                if ( $_current_item.hasClass( 'accrdion-active' ) ) {
+                                    $_current_item.removeClass( 'accrdion-active' );
+                                }
+
+                                $_current_item.find( '.accrdion-content' ).slideUp( configuration.toggleSpeed );
+                            }
+                        }
+                    });
+                } else {
+                    $_flag = true;
+                }
+
+                if ( $_flag ) {
+                    $_item.toggleClass( 'accrdion-active' );
+                    $_clicked.next().toggle( configuration.toggleSpeed );
+                }
             });
         });
 
     }
-}( jQuery ));
+})( jQuery );
